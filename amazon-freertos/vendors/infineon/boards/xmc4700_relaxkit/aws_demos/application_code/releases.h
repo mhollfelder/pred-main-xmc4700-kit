b523b59a922dfe5d3d0b0a954ef93235cd05aa3a
Manuel Hollfelder <manuel.hollfelder@infineon.com>
2023-02-10 16:43:15 +0100

    Added header file to manage versions

---
+++ amazon-freertos/vendors/infineon/boards/xmc4700_relaxkit/aws_demos/application_code/releases.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2021 Infineon Technologies AG.
+ *
+ * Licensed under the EVAL_XMC47_PREDMAIN_AA Evaluation Software License
+ * Agreement V1.0 (the "License"); you may not use this file except in
+ * compliance with the License.
+ *
+ * For receiving a copy of the License, please refer to:
+ *
+ * https://github.com/Infineon/pred-main-xmc4700-kit/LICENSE.txt
+ *
+ * Licensee acknowledges that the Licensed Items are provided by Licensor free
+ * of charge. Accordingly, without prejudice to Section 9 of the License, the
+ * Licensed Items provided by Licensor under this Agreement are provided "AS IS"
+ * without any warranty or liability of any kind and Licensor hereby expressly
+ * disclaims any warranties or representations, whether express, implied,
+ * statutory or otherwise, including but not limited to warranties of
+ * workmanship, merchantability, fitness for a particular purpose, defects in
+ * the Licensed Items, or non-infringement of third parties' intellectual
+ * property rights.
+ *
+ */
+
+#ifndef APPLICATION_CODE_RELEASES_H_
+#define APPLICATION_CODE_RELEASES_H_
+
+/* Firmware and hardware release version */
+#define releaseFIRMWARE_VERSION				"1.2.1"
+
+#define	releaseHARDWARE_VERSION_MAJOR		1 /* The major version, e.g. in 1.2.3 it is 1 */
+#define	releaseHARDWARE_VERSION_MINOR		3 /* The minor version, e.g. in 1.2.3 it is 2 */
+#define	releaseHARDWARE_VERSION_PATCH		0 /* The patch version, e.g. in 1.2.3 it is 3 */
+
+#endif /* APPLICATION_CODE_RELEASES_H_ */
